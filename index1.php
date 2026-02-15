729dab28969390f35071b928357cd9a3a4af6e8a
lon40seminarium-michal-lothamer <michal.lothamer@gmail.com>
2026-02-15 16:50:09 +0100

    1

--- index1.php
+++ index1.php
@@ -8,48 +8,48 @@
     <h1 class="title">Elementy Elektroniczne</h1>
 
     <img class="srodek" src="img/1.png" width="100">
-    <a class="link-srodek" href="subhuman/1.html" title="1">Rezystor</a><br><br>
+    <a class="link-srodek" href="subhuman/1.php" title="1">Rezystor</a><br><br>
 
     <img class="srodek" src="img/2.png" width="100">
-    <a class="link-srodek" href="subhuman/2.html" title="1">Kondensator</a><br><br>
+    <a class="link-srodek" href="subhuman/2.php" title="1">Kondensator</a><br><br>
 
     <img class="srodek" src="img/3.png" width="100">
-    <a class="link-srodek" href="subhuman/3.html" title="1">Dioda LED</a><br><br>
+    <a class="link-srodek" href="subhuman/3.php" title="1">Dioda LED</a><br><br>
 
     <img class="srodek" src="img/4.png" width="100">
-    <a class="link-srodek" href="subhuman/4.html" title="1">Wyświetlacz siedmiosegmentowy</a><br><br>
+    <a class="link-srodek" href="subhuman/4.php" title="1">Wyświetlacz siedmiosegmentowy</a><br><br>
 
     <img class="srodek" src="img/5.png" width="100">
-    <a class="link-srodek" href="subhuman/5.html" title="1">Klawiatura alfanumeryczna 4x4</a><br><br>
+    <a class="link-srodek" href="subhuman/5.php" title="1">Klawiatura alfanumeryczna 4x4</a><br><br>
 
     <img class="srodek" src="img/6.png" width="100">
-    <a class="link-srodek" href="subhuman/6.html" title="1">Mikroswitch</a><br><br>
+    <a class="link-srodek" href="subhuman/6.php" title="1">Mikroswitch</a><br><br>
 
     <img class="srodek" src="img/7.png" width="100">
-    <a class="link-srodek" href="subhuman/7.html" title="1">Fotorezystor</a><br><br>
+    <a class="link-srodek" href="subhuman/7.php" title="1">Fotorezystor</a><br><br>
 
     <img class="srodek" src="img/8.png" width="100">
-    <a class="link-srodek" href="subhuman/8.html" title="1">Przekaźnik</a><br><br>
+    <a class="link-srodek" href="subhuman/8.php" title="1">Przekaźnik</a><br><br>
 
     <img class="srodek" src="img/9.png" width="100">
-    <a class="link-srodek" href="subhuman/9.html" title="1">Silnik krokowy</a><br><br>
+    <a class="link-srodek" href="subhuman/9.php" title="1">Silnik krokowy</a><br><br>
 
     <img class="srodek" src="img/10.png" width="100">
-    <a class="link-srodek" href="subhuman/10.html" title="1">Servo SG-90</a><br><br>
+    <a class="link-srodek" href="subhuman/10.php" title="1">Servo SG-90</a><br><br>
 
     <img class="srodek" src="img/11.png" width="100">
-    <a class="link-srodek" href="subhuman/11.html" title="1">Potencjometr</a><br><br>
+    <a class="link-srodek" href="subhuman/11.php" title="1">Potencjometr</a><br><br>
 
     <img class="srodek" src="img/12.png" width="100">
-    <a class="link-srodek" href="subhuman/12.html" title="1">Enkoder inkrementalny</a><br><br>
+    <a class="link-srodek" href="subhuman/12.php" title="1">Enkoder inkrementalny</a><br><br>
 
     <img class="srodek" src="img/13.png" width="100">
-    <a class="link-srodek" href="subhuman/13.html" title="1">Czujnik ultradźwiękowy</a><br><br>
+    <a class="link-srodek" href="subhuman/13.php" title="1">Czujnik ultradźwiękowy</a><br><br>
 
     <img class="srodek" src="img/14.png" width="100">
-    <a class="link-srodek" href="subhuman/14.html" title="1">Czujnik magnetyczny - kontaktron</a><br><br>
+    <a class="link-srodek" href="subhuman/14.php" title="1">Czujnik magnetyczny - kontaktron</a><br><br>
 
     <img class="srodek" src="img/15.png" width="100">
-    <a class="link-srodek" href="subhuman/15.html" title="1">Wyświetlacz LCD</a><br><br>
+    <a class="link-srodek" href="subhuman/15.php" title="1">Wyświetlacz LCD</a><br><br>
 
 </html>
