fede3ad71cdcf7de45975ce087ed68495b290655
lon40seminarium-michal-lothamer <michal.lothamer@gmail.com>
2026-02-15 23:09:08 +0100

    1

--- index.php
+++ index.php
@@ -20,6 +20,9 @@
   <div class="container">
     <a class="button" href="index2.php" title="1">Obliczanie pól</a>
   </div>
+<a href="https://en.wikipedia.org/wiki/Vivlos">
+    <img src="img/statement.gif" alt="viv" class="center-img">
+</a>
 </body>
     </body>
 </html>
