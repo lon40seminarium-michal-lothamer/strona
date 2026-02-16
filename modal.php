8373a8f4691b86aa2f8b35e2cd71adeb0fc8933d
lon40seminarium-michal-lothamer <michal.lothamer@gmail.com>
2026-02-16 20:59:51 +0100

    1

---
+++ modal.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body style="font-family: Tahoma;"></body>
+        <meta charset="utf-8">
+        <title>strona</title>
+        <link rel="stylesheet" href="stylemodal.css">
+        <script src="scriptmodal.js"></script>
+    </head>
+    <body>
+        <button data-modal-target="#modal">Open Modal</button>
+        <div class="modal" id="modal">
+            <div class="modal_header">
+                <div class="modal_title">Modal Title</div>
+                <button data-close-button class="close_button">&times;</button>
+            </div>
+            <div class="modal_body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellendus error atque voluptatibus impedit velit, saepe illum quae eveniet quam. Exercitationem rem id nulla quaerat laudantium magnam quibusdam autem voluptates architecto, assumenda adipisci a voluptatibus eos porro molestiae animi odit nisi sit blanditiis necessitatibus omnis! Illo, nulla. Odit veritatis doloribus ratione, in libero, eligendi ea quibusdam unde veniam provident est modi quisquam. Rem expedita ad corrupti iure excepturi illo beatae exercitationem laboriosam amet neque. Perferendis ut debitis voluptatem cupiditate magnam!
+        
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </body>
+</html>
